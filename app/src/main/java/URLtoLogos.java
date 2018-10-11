ad60a8eff3cb2b3a3c6f7919c2e01188cbd16fe7
cmao522 <cmao@uncc.edu>
2018-10-11 13:23:11 -0400

    commit

---
+++ app/src/main/java/URLtoLogos.java
@@ -0,0 +1,9 @@
+public class URLtoLogos {
+    String small, medium, large;
+
+    public URLtoLogos(String sm, String md, String lrg) {
+        this.small = sm;
+        this.medium = md;
+        this.large = lrg;
+    }
+}
